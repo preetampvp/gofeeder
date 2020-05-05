c799f5b0833f1212a591846b09adc57d6d390025
preetam <preetamvp@hotmail.com>
2020-05-05 10:37:19 +1000

    refactor building url to cater for absolute and relative paths

--- feed/scraper.go
+++ feed/scraper.go
@@ -2,6 +2,9 @@ package feed
 
 import (
 	"fmt"
+	"net/url"
+	"path"
+	"strings"
 
 	"github.com/gocolly/colly"
 	"github.com/preetampvp/gofeeder/config"
@@ -18,6 +21,23 @@ type scraper struct {
 	pageIndex int
 }
 
+type buildParam struct {
+	link       string
+	forceBuild bool
+}
+
+func (s *scraper) buildUrl(param buildParam) string {
+
+	if !strings.HasPrefix(param.link, "http") || param.forceBuild {
+		url, _ := url.Parse(s.config.UrlBase)
+		url.Path = path.Join(url.Path, param.link)
+		url, _ = url.Parse(url.Path)
+		return url.String()
+	}
+
+	return param.link
+}
+
 func (s *scraper) GetFeedName() string {
 	return fmt.Sprintf("%s", s.config.Name)
 }
@@ -29,7 +49,7 @@ func (s *scraper) GetPageIndex() int {
 func (s *scraper) GetInitialFeed() chan Feed {
 	s.pageIndex = 1
 	s.prevPaths = make([]string, 0)
-	initialPath := fmt.Sprintf("%s%s", s.config.UrlBase, s.config.UrlPath)
+	initialPath := s.buildUrl(buildParam{link: s.config.UrlPath, forceBuild: true})
 	s.prevPaths = append(s.prevPaths, initialPath)
 	return s.scrapeFeed(initialPath)
 }
@@ -40,15 +60,11 @@ func (s *scraper) GetNextFeed() chan Feed {
 	}
 
 	s.pageIndex += 1
-	if s.nextPath != "" && len(s.prevPaths) < s.pageIndex {
+	if len(s.prevPaths) < s.pageIndex {
 		s.prevPaths = append(s.prevPaths, s.nextPath)
 	}
 
-	if s.config.NextLinkIsRelative {
-		return s.scrapeFeed(fmt.Sprintf("%s%s", s.config.UrlBase, s.nextPath))
-	}
-
-	return s.scrapeFeed(fmt.Sprintf("%s", s.nextPath))
+	return s.scrapeFeed(s.nextPath)
 }
 
 func (s *scraper) GetPrevFeed() chan Feed {
@@ -56,33 +72,30 @@ func (s *scraper) GetPrevFeed() chan Feed {
 		s.pageIndex -= 1
 	}
 
-	if s.config.NextLinkIsRelative {
-		return s.scrapeFeed(fmt.Sprintf("%s%s", s.config.UrlBase, s.prevPaths[s.pageIndex-1]))
-	}
-
 	return s.scrapeFeed(fmt.Sprintf("%s", s.prevPaths[s.pageIndex-1]))
 }
 
 func (s *scraper) scrapeFeed(url string) chan Feed {
 	ch := make(chan Feed)
+	s.nextPath = ""
 
 	go func() {
 		collector := colly.NewCollector()
 		defer close(ch)
 
 		collector.OnHTML(s.config.LinkSelector, func(e *colly.HTMLElement) {
-
 			title := e.Text
-			url := e.Attr("href")
-
+			url := s.buildUrl(buildParam{link: e.Attr("href")})
 			ch <- Feed{Title: title, Link: url}
 		})
 
 		if s.config.NextPageSelector != "" {
 			collector.OnHTML(s.config.NextPageSelector, func(e *colly.HTMLElement) {
-
 				url := e.Attr("href")
 				s.nextPath = url
+				if s.nextPath != "" {
+					s.nextPath = s.buildUrl(buildParam{link: url})
+				}
 			})
 		}
 
